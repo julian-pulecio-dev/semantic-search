927353f6a925b7077fee5548bf40ea689437b643
Julian Pulecio <julian.pulecio1@gmail.com>
2026-02-05 17:14:57 -0500

    black applied"

--- app/app/settings.py
+++ app/app/settings.py
@@ -21,9 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-!m967pxhhdbuv#k2w^^2$rh&_8hj^dbci%7av%rf)1#+$d6u*m"
-)
+SECRET_KEY = "django-insecure-!m967pxhhdbuv#k2w^^2$rh&_8hj^dbci%7av%rf)1#+$d6u*m"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- app/app/urls.py
+++ app/app/urls.py
@@ -27,7 +27,6 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView,
 )
 
-
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
@@ -41,19 +40,7 @@ urlpatterns = [
         SpectacularRedocView.as_view(url_name="schema"),
         name="redoc",
     ),
-    path(
-        'api/token/',
-        TokenObtainPairView.as_view(),
-        name='token_obtain_pair'
-    ),
-    path(
-        'api/token/refresh/',
-        TokenRefreshView.as_view(),
-        name='token_refresh'
-    ),
-    path(
-        'api/token/verify/',
-        TokenVerifyView.as_view(),
-        name='token_verify'
-    ),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
 ]
