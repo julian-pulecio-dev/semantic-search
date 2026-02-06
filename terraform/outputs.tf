a9f97f0ac808a5d7853c5cea5aeaecea1e395642
Julian Pulecio <julian.pulecio1@gmail.com>
2026-02-06 16:09:25 -0500

    terraform outputs updated

---
+++ terraform/outputs.tf
@@ -0,0 +1,19 @@
+output "ecs_cluster_name" {
+  value = module.ecs_cluster.name
+}
+
+output "ecs_service_name" {
+  value = module.ecs_service.name
+}
+
+output "migration_task_definition" {
+  value = module.ecs_task_definition_migrate.arn
+}
+
+output "subnet_ids" {
+  value = module.vpc_subnets.subnet_ids
+}
+
+output "ecs_security_group" {
+  value = module.vpc_security_group.security_group_id
+}
