d5daec01b5e521fbb9bd568b614694dca2a81ebe
Julian Pulecio <julian.pulecio1@gmail.com>
2026-02-10 22:33:10 -0500

    documents field added to user model

--- app/core/models.py
+++ app/core/models.py
@@ -48,10 +48,12 @@ class User(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         return self.email
 
+
 class Document(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     url = models.URLField()
     uploaded_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="documents")
 
     def __str__(self):
         return self.url
