7ce3d9c31b473684550bab3c7f82d066d80558a0
Julian Pulecio <julian.pulecio1@gmail.com>
2026-02-19 23:29:13 -0500

    .github\workflows\checks updated to handle a postgress db

--- app/app/settings/test.py
+++ app/app/settings/test.py
@@ -1,11 +1,13 @@
 from .base import *
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent.parent
+import os
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": os.environ.get("DB_PORT"),
     }
 }
