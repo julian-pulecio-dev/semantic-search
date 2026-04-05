a7aa49dd9f8c92cc6f68d53d6c75232b9e5c442e
Julian Pulecio <julian.pulecio1@gmail.com>
2026-04-04 22:43:23 -0500

    UploadSessionSerializer upaded

--- app/upload_session/serializers.py
+++ app/upload_session/serializers.py
@@ -1,11 +1,30 @@
 from rest_framework import serializers
 from upload_session.models import UploadSession
 from document.models import Document
-from drf_spectacular.utils import extend_schema_field, inline_serializer
 
 
 class CreateUploadSessionSerializer(serializers.Serializer):
     document_type = serializers.CharField(max_length=100)
+
+
+class PresignedPostFieldsSerializer(serializers.Serializer):
+    key = serializers.CharField(help_text="S3 object key")
+    Content_Type = serializers.CharField(source="Content-Type")
+    acl = serializers.CharField()
+    x_amz_server_side_encryption = serializers.CharField(source="x-amz-server-side-encryption")
+    x_amz_meta_upload_session_id = serializers.UUIDField(source="x-amz-meta-upload-session-id")
+    x_amz_meta_document_id = serializers.UUIDField(source="x-amz-meta-document-id")
+    x_amz_meta_user_id = serializers.UUIDField(source="x-amz-meta-user-id")
+    AWSAccessKeyId = serializers.CharField()
+    policy = serializers.CharField()
+    signature = serializers.CharField()
+
+
+class PresignedPostUrlSerializer(serializers.Serializer):
+    url = serializers.URLField(help_text="S3 presigned POST endpoint")
+    fields = PresignedPostFieldsSerializer()
+    upload_session_id = serializers.UUIDField()
+    document_id = serializers.UUIDField()
 
 
 class UploadSessionSerializer(serializers.Serializer):
@@ -13,31 +32,7 @@ class UploadSessionSerializer(serializers.Serializer):
         source="upload_session_id", read_only=True
     )
     document_id = serializers.UUIDField(read_only=True)
-    url = extend_schema_field(
-        inline_serializer(
-            name="PresignedPostUrl",
-            fields={
-                "url": serializers.URLField(help_text="S3 presigned POST endpoint"),
-                "fields": inline_serializer(
-                    name="PresignedPostFields",
-                    fields={
-                        "key": serializers.CharField(help_text="S3 object key"),
-                        "Content-Type": serializers.CharField(),
-                        "acl": serializers.CharField(),
-                        "x-amz-server-side-encryption": serializers.CharField(),
-                        "x-amz-meta-upload-session-id": serializers.UUIDField(),
-                        "x-amz-meta-document-id": serializers.UUIDField(),
-                        "x-amz-meta-user-id": serializers.UUIDField(),
-                        "AWSAccessKeyId": serializers.CharField(),
-                        "policy": serializers.CharField(),
-                        "signature": serializers.CharField(),
-                    },
-                ),
-                "upload_session_id": serializers.UUIDField(),
-                "document_id": serializers.UUIDField(),
-            },
-        )
-    )(serializers.JSONField())
+    url = PresignedPostUrlSerializer()
     status = serializers.CharField(read_only=True)
     expires_at = serializers.DateTimeField(read_only=True)
 
