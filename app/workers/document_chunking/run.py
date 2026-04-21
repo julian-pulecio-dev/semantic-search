9e438f5cb7be49243b1a8366a7bbb62c5d60fef2
Julian Pulecio <julian.pulecio1@gmail.com>
2026-04-21 01:01:46 -0500

    tests updated

--- app/workers/document_chunking/run.py
+++
@@ -1,6 +0,0 @@
-from workers.worker_runner import WorkerRunner
-
-if __name__ == "__main__":
-    runner = WorkerRunner()
-    from workers.document_chunking.handler import DocumentChunkingHandler
-    runner.run(processor_cls=DocumentChunkingHandler)
--- app/workers/embedding/run.py
+++
@@ -1,7 +0,0 @@
-from workers.worker_runner import WorkerRunner
-
-if __name__ == "__main__":
-    from workers.embedding.handler import EmbeddingHandler
-
-    runner = WorkerRunner()
-    runner.run(processor_cls=EmbeddingHandler)
